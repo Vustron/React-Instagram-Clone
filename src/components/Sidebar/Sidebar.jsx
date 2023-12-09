e811ad060ea5e17a4c5c54ad3222aef65167c0d7
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2023-12-09 15:35:13 +0800

    Added Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,148 @@
+import { Avatar, Box, Center, Flex, Link, Tooltip } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import {
+    CreatePostLogo,
+    InstagramLogo,
+    InstagramMobileLogo,
+    NotificationsLogo,
+    SearchLogo
+} from "../../assets/constants";
+import { AiFillHome } from "react-icons/ai";
+import { BiLogOut } from "react-icons/bi";
 
-
+// sidebar
 const Sidebar = () => {
-    return (
-        <div>Sidebar</div>
-    )
-}
+    // sidebar items
+    const sidebarItems = [
+        {
+            icon: <AiFillHome size={25} />,
+            text: "Home",
+            link: "/",
+        },
+        {
+            icon: <SearchLogo />,
+            text: "Search",
+        },
+        {
+            icon: <NotificationsLogo />,
+            text: "Notification",
+        },
+        {
+            icon: <CreatePostLogo />,
+            text: "Create",
+        },
+        {
+            icon: <Avatar size={"sm"} name='Vustron Vustronus' src="/profilepic.png" />,
+            text: "Profile",
+            link: "/asaprogrammer",
+        },
+    ];
+
+    return <Box
+        height={"100vh"}
+        borderRight={"1px solid"}
+        borderColor={"whiteAlpha.300"}
+        py={8}
+        position={"sticky"}
+        top={0}
+        left={0}
+        px={{ base: 2, md: 4 }}>
+
+        <Flex direction={"column"} gap={10} w={"full"} height={"full"}>
+            {/* instagram logo */}
+            <Link
+                to={"/"}
+                as={RouterLink}
+                pl={2}
+                display={{ base: "none", md: "block" }}
+                cursor={"pointer"}
+                alignItems={"center"}
+                justifyContent={"center"}
+            >
+                <InstagramLogo />
+            </Link>
+            {/* instagram mobile logo */}
+            <Link
+                to={"/"}
+                as={RouterLink}
+                p={2}
+                display={{ base: "block", md: "none" }}
+                cursor={"pointer"}
+                borderRadius={6}
+                _hover={{
+                    bg: "whiteAlpha.200"
+                }}
+                w={10}
+                alignItems={"center"}
+                justifyContent={"center"}
+                m={1}
+            >
+                <InstagramMobileLogo />
+            </Link>
+            {/* sidebaritems */}
+            <Flex direction={"column"} gap={5} cursor={"pointer"} >
+                {sidebarItems.map((item, index) => (
+                    <Tooltip
+                        key={index}
+                        hasArrow
+                        label={item.text}
+                        placement="right"
+                        ml={1}
+                        openDelay={500}
+                        display={{ base: "block", md: "none" }}
+                    >
+                        <Link
+                            display={"flex"}
+                            to={item.link || null}
+                            as={RouterLink}
+                            gap={4}
+                            _hover={{ bg: "whiteAlpha.400" }}
+                            borderRadius={6}
+                            p={2}
+                            w={{ base: 10, md: "full" }}
+                            justifyContent={{ base: "center", md: "flex-start" }}
+                            alignItems={"center"}
+                            m={1}
+                        >
+                            {item.icon}
+                            <Box display={{ base: "none", md: "block" }}>
+                                {item.text}
+                            </Box>
+                        </Link>
+                    </Tooltip>
+                ))}
+            </Flex>
+            {/* Logout */}
+            <Tooltip
+                hasArrow
+                label={"Logout"}
+                placement="right"
+                ml={1}
+                openDelay={500}
+                display={{ base: "block", md: "none" }}
+            >
+                <Link
+                    display={"flex"}
+                    to={"/auth"}
+                    as={RouterLink}
+                    gap={4}
+                    _hover={{ bg: "whiteAlpha.400" }}
+                    borderRadius={6}
+                    p={2}
+                    w={{ base: 10, md: "full" }}
+                    justifyContent={{ base: "center", md: "flex-start" }}
+                    alignItems={"center"}
+                    m={1}
+                    mt={"auto"}
+                >
+                    <BiLogOut size={25} />
+                    <Box display={{ base: "none", md: "block" }}>
+                        Logout
+                    </Box>
+                </Link>
+            </Tooltip>
+        </Flex>
+    </Box>;
+};
 
 export default Sidebar
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -4,20 +4,21 @@ import AuthForm from "../../components/AuthForm/AuthForm";
 
 const AuthPage = () => {
     return (
-        <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
+        <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4} pr={200}>
             <Container maxW={"container.md"} padding={0}>
                 <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
 
                     {/* Left Side */}
                     <Box display={{ base: "none", md: "block" }}>
-                        <Image src="/auth.png" h={650} alt='Phone img' />
+                        <Image src="/auth.png" h={600} alt='Phone img' />
                     </Box>
 
                     {/* Right Side */}
                     <VStack spacing={4} align={"stretch"}>
+                        {/* Authform inputs */}
                         <AuthForm />
                         <Box textAlign={"center"}>
-                            Get the app.
+                            Get the app
                         </Box>
                         <Flex gap={5} justifyContent={"center"}>
                             <Image src='/playstore.png' h={"10"} alt='Playstore logo' />
